c4dbb8949b0a563df78c6b39b5d06705df105544
Christopher Martin <ch.martin@gmail.com>
2012-08-13 21:45:31 -0400

    create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+  name = 'Grouch',
+  version = '0.1',
+  description = 'Scraping for Oscar',
+  author = 'Christopher Martin',
+  author_email = 'ch.martin@gmail.com',
+  url = 'https://github.com/chris-martin/grouch',
+  packages = [ 'grouch' ],
+)
