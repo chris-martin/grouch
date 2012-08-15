7b3d18fc7902fde01d6fefd2a2cfad289b4d2be6
Christopher Martin <ch.martin@gmail.com>
2012-08-14 23:34:18 -0400

    add test case for Scraper.get_subjects

--- grouch/tests/scraper_tests.py
+++ grouch/tests/scraper_tests.py
@@ -1,5 +1,4 @@
-from grouch import Context, Scraper, Term
-from pprint import pprint
+from grouch import Context, Scraper, Subject, Term
 
 def file_path(name):
   return 'grouch/tests/%s' % name
@@ -22,4 +21,17 @@ def test_get_terms():
     ( Term(Term.SUMMER, 2012), '201205' ),
     ( Term(Term.SPRING, 2012), '201202' ),
     ( Term(Term.FALL, 2011), '201108' ),
+  ]
+
+def test_get_subjects():
+  scraper = create_scraper()
+  html = read('bwckgens.p_proc_term_date.html')
+  subjects = scraper.get_subjects(html = html)
+  assert subjects == [
+    Subject('AE', 'Aerospace Engineering'),
+    Subject('CSE', 'Computational Science & Engr'),
+    Subject('CS', 'Computer Science'),
+    Subject('NRE', 'Nuclear & Radiological Engr'),
+    Subject('SOC', 'Sociology'),
+    Subject('SPAN', 'Spanish'),
   ]
