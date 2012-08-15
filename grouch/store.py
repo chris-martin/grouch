299951e2bf1a5d210749eb1f4c3582318d26c276
Christopher Martin <ch.martin@gmail.com>
2012-08-14 23:04:44 -0400

    privatize members of Store

--- grouch/store.py
+++ grouch/store.py
@@ -8,13 +8,13 @@ class Store:
     if context is None:
       context = Context()
 
-    self.context = context
-    self.private_scrapers = {}
-    self.public_scraper = Scraper(context = self.context)
-    self.data = {}
+    self.__context = context
+    self.__private_scrapers = {}
+    self.__public_scraper = Scraper(context = context)
+    self.__data = {}
 
   def add_private_scraper(username, password):
-    self.private_scrapers[username] = Scraper(
+    self.__private_scrapers[username] = Scraper(
       context = self.context,
       username = username,
       password = password,
@@ -24,10 +24,10 @@ class Store:
   # A list of Terms, sorted by chronology in reverse.
   #
   def get_terms(self):
-    data = self.data
+    data = self.__data
 
     if not 'term list' in data:
-      terms = self.public_scraper.get_terms()
+      terms = self.__public_scraper.get_terms()
       data['term list'] = list([ t[0] for t in terms ])
       data['term list'].sort(reverse = True)
       data['term dict'] = dict([ (t[0], { 'id': t[1] }) for t in terms ])
@@ -38,7 +38,7 @@ class Store:
   # A list of Subjects, sorted by name.
   #
   def get_subjects(self, term = None):
-    data = self.data
+    data = self.__data
     self.get_terms()
 
     if term is None:
@@ -47,7 +47,7 @@ class Store:
     t = data['term dict'][term]
 
     if 'subject list' not in t:
-      t['subject list'] = self.public_scraper.get_subjects(term_id = t['id'])
+      t['subject list'] = self.__public_scraper.get_subjects(term_id = t['id'])
       t['subject list'].sort(key = lambda x: x.get_name())
 
     return t['subject list']
